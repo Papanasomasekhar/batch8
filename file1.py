b24b9646d9664d35a5bfbcc77a9305cf0ff7118d
Papanasomasekhar <162982487+Papanasomasekhar@users.noreply.github.com>
2024-03-11 16:04:08 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,71 @@
+'''
+#print("hello")
+#print("hello 1")
+"""
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+"""
+
+---->commenting
+1.)commenting is used for making documentation in python file
+2.)when the line is commented,that line will not be considered as a part of the code
+3.)when the program is executed,commented the line will not be in the part of execution
+---->NOTE:
+    single line commenting can be done using # at the begining of the
+'''
+import this
+print(this)
+
+Bargained characters
+  Controls and Data structures-->ABC language
+  Strings and its formats-->from C
+  Regular expressions-->Pearl
+  Exception handling-->Module 3
+  Magic metods from-->fortron
+
+  -->Flavours of Python
+  >Cpython--Written in C,Most common I,plementation of python.
+  > Jython--Java implementation of Python.
+  >Ironpython--implementation of the python programming language which is tightly integrated with NET.
+  >Brython--browserpython,runs in browser.
+  >Rubypython--Bridge between python and Ruby
+
+# ----> print(object, sep, end, file, flush)
+# print has 5 parameters
+
+# object  -->the value to be printed is object
+# sep--> seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+# print(a,b,c, sep='$')
+
+# end --> which is used to add the string at the end of the value,
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
